52469d6ab7289f8a647c7e06c4e7de3d56ef0a7f
XinLei <93850171+X-L-Liu@users.noreply.github.com>
2024-09-16 17:46:46 +0800

    Update unzip.py

--- unzip.py
+++ unzip.py
@@ -1,5 +1,5 @@
 import zipfile
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
     with zipfile.ZipFile('SAU_Net/cifar10/SAU_Net_MAPE_0.9594.zip') as zip_file:
         zip_file.extractall('SAU_Net/cifar10')
